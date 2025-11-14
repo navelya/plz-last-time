4bf2a8eacd723c563f661152453fab57d5836fc4
Naveh Elya <102157763+navelya@users.noreply.github.com>
2025-11-14 21:09:00 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/consulting-services/',
+  base: '/${repoName}/',
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src')
+    }
+  }
 })
